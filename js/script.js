c34a7779f716cae86e33b4c01292c892914f4cd3
MateuszKacperski <kacperskimateusz95@gmail.com>
2024-01-24 17:21:24 +0100

    added dinamic contacts

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 console.log('Vue ok', Vue);
+
+const {createApp} = Vue;
+const app = createApp({
+    name: 'Vue Slider',
+    data: () => ({
+        data,
+    }),
+    
+})
+
+console.log(data);
+app.mount('#root')
