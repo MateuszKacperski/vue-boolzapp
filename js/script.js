ec04641260d3e0c9ea36411c54b0a23e751e563c
MateuszKacperski <kacperskimateusz95@gmail.com>
2024-01-25 11:56:53 +0100

    fixing

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
 console.log('Vue ok', Vue);
 
+
+const {user, contacts} = data
+
+
 const {createApp} = Vue;
 const app = createApp({
     name: 'Vue Slider',
     data: () => ({
-        data,
+        user,
+        contacts,
+        activeId: 1,
     }),
+    computed:{
+        currentContact(){
+            if(this.contacts.id !== this.activeId){
+                return this.activeId = this.contacts.id
+            }else{
+                return this.activeId
+            } 
+        }
+    },
+    methods:{
+        getAvatarUrl({avatar}){
+            return `img/avatar${avatar}.jpg`
+        }
+    }
     
 })
 
